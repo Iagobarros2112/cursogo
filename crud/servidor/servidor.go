91c7bb1f921d5e84776c049957ec97eedb48296e
Iagobarros <iagobarrosgomes2112@gmail.com>
2025-04-27 13:01:45 -0300

    criei a funçao buscar usuario e buscar usuarios do crud principal

--- crud/servidor/servidor.go
+++ crud/servidor/servidor.go
@@ -62,5 +62,38 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// achar todo usuarios no database
-func BuscarUsuarios()
+// achar todos usuarios no database
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("erro ao conectar com o database!!!!"))
+
+	}
+
+	defer db.Close()
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nul {
+		w.Write([]byte("erro ao buscar os usuarios"))
+	}
+
+	defer linhas.Close()
+
+	//essa parte eh necessaria pois a api prescisa
+	//  reorganizar os dados novamente para que seja
+	//  readble para outro usuario no outro endpoint
+
+	var usuarios [] usuario
+	for linhas.Next() {
+		var usuario usuario 
+
+		if erro := linhas.Scan(&usuario.ID, &usuario.Nome, &usuario.Email)
+	}
+
+
+}
+
+// achar um usuario em expecifico no database
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
