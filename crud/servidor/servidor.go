8bb9a5e1e0a4bb0bf7202822c9a90279a7905a8d
Iagobarros <iagobarrosgomes2112@gmail.com>
2025-05-05 12:08:05 -0300

    terminei a funçao atualizar usuario

--- crud/servidor/servidor.go
+++ crud/servidor/servidor.go
@@ -157,4 +157,40 @@ func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("erro ao converter o parametro para inteiro"))
 		return
 	}
+
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("erro ao ler o corpo da requisiçao"))
+		return
+	}
+
+	var usuario usuario
+	if erro := json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		w.Write([]byte("erro ao converter o usuario para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("erro ao conectar no banco de dados!!!"))
+		return
+	}
+
+	defer db.Close()
+
+	statement, erro := db.Prepare("update usuarios set nome = ?, email = ? where id = ?")
+	if erro != nil {
+		w.Write([]byte("erro ao criar o statement"))
+		return
+	}
+
+	defer statement.Close()
+
+	if _, erro := statement.Exec(usuario.Nome, usuario.Email, ID); erro != nil {
+		w.Write([]byte("erro ao atualizar o usuario"))
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
 }
